2d41a783648824421e1062a097e383171a07cda1
andre4ik3 <andrey@andre4ik3.email>
2022-02-03 23:02:46 +0300

    Initial Commit

---
+++ Fortnite/src/__main__.py
@@ -0,0 +1,25 @@
+# Fortnite Container Entry Point
+# Copyright (C) 2022  andre4ik3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import zmq
+from Client import AerialClient
+
+context = zmq.Context()
+socket = context.socket(zmq.PULL)
+
+# Connect to socket
+socket.connect("tcp://main:3000")
+print("Connected to ZeroMQ socket")
